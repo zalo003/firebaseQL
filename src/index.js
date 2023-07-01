8521884564a106ba51c8cb9f8d242047dc418e67
Solomon Gbadamosi <exampaqs@gmail.com>
2023-07-02 02:28:56 +0800

    added index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { StorageUpload } from './filemanger/FirebaseStorage'
+import {BaseModel} from './models/Base.model'
+import { Users } from './models/Users.model'
+
+
+
+module.exports = {
+    BaseModel,
+    Users,
+    StorageUpload
+}
