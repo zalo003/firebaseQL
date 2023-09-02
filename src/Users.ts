9c1863d133013d88911b4495ecae0655d0de3ea5
Solomon Gbadamosi <exampaqs@gmail.com>
2023-09-02 06:40:22 +0100

    add email verification to login

--- src/Users.ts
+++ src/Users.ts
@@ -64,7 +64,9 @@ export class Users extends BaseModel {
      * @param { string , string, boolean} credentials - login credentials 
      * @returns {Promise<QueryReturn>}
      */
-     async login ({email, password, auth, persist = false}: {email: string, password: string, auth: Auth,  persist?: boolean}): Promise<QueryReturn> {
+     async login ({email, password, auth, persist = false, verifyEmail = false}: 
+        {email: string, password: string, auth: Auth,  persist?: boolean, verifyEmail?: boolean}): Promise<QueryReturn> {
+
         let result : QueryReturn = {result: undefined, status: false}
         
         try {
@@ -75,9 +77,15 @@ export class Users extends BaseModel {
             // verify user's email and password is correct
             const userAuth = await signInWithEmailAndPassword(auth, email, password)
             if(userAuth){
-                result.status = true
-                // find user in table
-                result.result =  userAuth.user
+                if(verifyEmail && !userAuth.user.emailVerified){
+                    await this.sendEmailVerification(userAuth.user)
+                    result.status = false
+                    result.error = "Email is not verified"
+                } else {
+                    result.status = true
+                    // find user in table
+                    result.result =  userAuth.user
+                }
             }
         } catch (error) {
             errorLogger("login error: ", error)
@@ -171,8 +179,8 @@ export class Users extends BaseModel {
     * create and validate users
     * @param param0 
     */
-   async loginWithMultiAuthFactor({email, password, auth, recaptcha, phoneNumber, persist = true}: 
-    {email: string, password: string, auth: Auth, recaptcha: ApplicationVerifier, phoneNumber?:string, persist?: boolean}): Promise<MFAVerifier | null>{
+   async loginWithMultiAuthFactor({email, password, auth, recaptcha, phoneNumber, persist = true, verifyEmail = false}: 
+    {email: string, password: string, auth: Auth, recaptcha: ApplicationVerifier, phoneNumber?:string, persist?: boolean, verifyEmail?: boolean}): Promise<MFAVerifier | null>{
         
         try {
             // persist user in session
@@ -182,6 +190,11 @@ export class Users extends BaseModel {
             // sign in user
             const credential = await signInWithEmailAndPassword(auth, email, password)
             if(credential){
+                // user is required to verify their email address
+                if(verifyEmail && !credential.user.emailVerified){
+                    await this.sendEmailVerification(credential.user)
+                    throw new Error("Email is not verified")
+                }
                 const user = phoneNumber? {...credential.user, phoneNumber}: credential.user
                 // user is not MFA enabled
                 // enroll user with multi-factor auth if user is admin
@@ -197,6 +210,11 @@ export class Users extends BaseModel {
                 return null
             }            
         } catch (e) {
+            // user needs to verify their email address
+            if(verifyEmail && !auth.currentUser?.emailVerified){
+                await this.sendEmailVerification(auth.currentUser!)
+                throw new Error("Email is not verified")
+            }
             errorLogger("loginWithMultiAuthFactor error: ", e)
             const error = e as any
             if (error.code === 'auth/multi-factor-auth-required') {
@@ -208,30 +226,30 @@ export class Users extends BaseModel {
         }
    }
 
- /**
+    /**
      * send OTP to enable 2 factor authentication
      * @param user 
      * @param recaptchaverifier 
      */
- private setMultiFactorEnrollment = async (user: User, recaptchaverifier: ApplicationVerifier, auth: Auth): Promise<string | null> =>{
-    try {
-        // get session
-        const multifactorSession = await multiFactor(user).getSession()
-        // specify the phone number and pass the MFA session
-        const phoneInfoOptions = {
-            phoneNumber: user.phoneNumber,
-            session: multifactorSession
-        };
-        const phoneAuthProvider = new PhoneAuthProvider(auth);
-        // Send SMS verification code.
-       return await phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaverifier);
-    } catch (e) {
-        errorLogger("setMultiFactorEnrollment error: ", e)
-        return null
+    private setMultiFactorEnrollment = async (user: User, recaptchaverifier: ApplicationVerifier, auth: Auth): Promise<string | null> =>{
+        try {
+            // get session
+            const multifactorSession = await multiFactor(user).getSession()
+            // specify the phone number and pass the MFA session
+            const phoneInfoOptions = {
+                phoneNumber: user.phoneNumber,
+                session: multifactorSession
+            };
+            const phoneAuthProvider = new PhoneAuthProvider(auth);
+            // Send SMS verification code.
+        return await phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaverifier);
+        } catch (e) {
+            errorLogger("setMultiFactorEnrollment error: ", e)
+            return null
+        }
     }
-}
 
-/**
+    /**
      * complete user signIn process with OTP
      * @param param0 
      * @param userCode 
@@ -239,61 +257,61 @@ export class Users extends BaseModel {
      * @param onSuccess 
      * @returns {User | null}
      */
-confirmOTP = async (verifier: MFAVerifier, userCode: string): Promise<User | null> =>{
-    try {
-        const { verificationId, resolver} = verifier
-         // Ask user for the verification code. Then:
-         const cred = PhoneAuthProvider.credential(verificationId, userCode);
-         const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(cred);
+    confirmOTP = async (verifier: MFAVerifier, userCode: string): Promise<User | null> =>{
+        try {
+            const { verificationId, resolver} = verifier
+            // Ask user for the verification code. Then:
+            const cred = PhoneAuthProvider.credential(verificationId, userCode);
+            const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(cred);
 
-         let user: User | null = null
-         // Complete enrollment.
-         if(resolver){
-            const credential = await resolver.resolveSignIn(multiFactorAssertion)
-            user = credential.user
-         } else {
-            if(verifier.user){
-                // set up multifactor authentication
-                await multiFactor(verifier.user).enroll(multiFactorAssertion, "primary phone")
-                user = verifier.user
-            } 
-         }
-         return user
-    } catch (error) {
-        // otp is not correct
-        errorLogger("confirmOTP error: ", error)
-        return null
-    }
-}
-
-/**
- * send OTP to member
- * @param resolver 
- * @param recaptchaVerifier 
- * @param setter 
- * @returns {MFAVerifier | null}
- */
-private sendOTP =  async (resolver: MultiFactorResolver, recaptchaVerifier: ApplicationVerifier, auth: Auth) : Promise<MFAVerifier | null> =>{
-    try {
-        if (resolver.hints[0].factorId ===
-            PhoneMultiFactorGenerator.FACTOR_ID) {
-            const phoneInfoOptions = {
-                multiFactorHint: resolver.hints[0],
-                session: resolver.session
-            };
-            const phoneAuthProvider = new PhoneAuthProvider(auth);
-            // Send SMS verification code
-            const verificationId = await phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
-            return {verificationId, resolver}
-        } else {
-            // other auth factor managed to be first 
+            let user: User | null = null
+            // Complete enrollment.
+            if(resolver){
+                const credential = await resolver.resolveSignIn(multiFactorAssertion)
+                user = credential.user
+            } else {
+                if(verifier.user){
+                    // set up multifactor authentication
+                    await multiFactor(verifier.user).enroll(multiFactorAssertion, "primary phone")
+                    user = verifier.user
+                } 
+            }
+            return user
+        } catch (error) {
+            // otp is not correct
+            errorLogger("confirmOTP error: ", error)
             return null
-        } 
-    } catch (e) {
-        errorLogger("sendOTP error: ", e)
-        return null
+        }
     }
-}
+
+    /**
+     * send OTP to member
+     * @param resolver 
+     * @param recaptchaVerifier 
+     * @param setter 
+     * @returns {MFAVerifier | null}
+     */
+    private sendOTP =  async (resolver: MultiFactorResolver, recaptchaVerifier: ApplicationVerifier, auth: Auth) : Promise<MFAVerifier | null> =>{
+        try {
+            if (resolver.hints[0].factorId ===
+                PhoneMultiFactorGenerator.FACTOR_ID) {
+                const phoneInfoOptions = {
+                    multiFactorHint: resolver.hints[0],
+                    session: resolver.session
+                };
+                const phoneAuthProvider = new PhoneAuthProvider(auth);
+                // Send SMS verification code
+                const verificationId = await phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
+                return {verificationId, resolver}
+            } else {
+                // other auth factor managed to be first 
+                return null
+            } 
+        } catch (e) {
+            errorLogger("sendOTP error: ", e)
+            return null
+        }
+    }
 
     /**
      * Handle email verification for user
@@ -335,5 +353,24 @@ private sendOTP =  async (resolver: MultiFactorResolver, recaptchaVerifier: Appl
     async deleteAccount(user: User) {
         await deleteUser(user)
         await this.delete(user.uid)
+    }
+
+    /**
+     * resend email verification for expired link
+     * @param auth 
+     * @returns 
+     */
+    async sendEmailVerification(user: User): Promise<string | null> {
+        // fetch current user from auth and resend email verification
+        try {
+            if(user){
+                sendEmailVerification(user)
+                return null
+            } else {
+                return "Unknown user account, please sign up!"
+            }
+        } catch (error) {
+            return "Unable to send verification, contact Administrators!"
+        }
     }
 }
